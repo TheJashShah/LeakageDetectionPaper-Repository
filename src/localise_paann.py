d070bce8bef35a29796eac7299a112238633b72b
JashShah <78263590+TheJashShah@users.noreply.github.com>
2026-08-27 09:46:49 +0530

    remove fixed path from code

--- src/localise_paann.py
+++ src/localise_paann.py
@@ -413,7 +413,7 @@ def run_realtime_dashboard(full_data, pressure_data, model, device):
 
 
 
-SCENARIO_ID = 156 
+SCENARIO_ID = None
 
 
 mean_stats, std_stats = load_normalization_stats(files_dir)
@@ -567,10 +567,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
 
-
-TEST_SCENARIOS = [921]  
-TEST_SCENARIOS= [3, 5, 11, 30, 49, 61, 64, 71, 77, 78, 80, 89, 93, 108, 109, 122, 123, 127, 140, 143, 156, 160, 164, 183, 190, 192, 231, 239, 248, 257, 262, 272, 286, 295, 300, 306, 311, 320, 325, 331, 332, 333, 351, 370, 430, 436, 447, 448, 466, 501, 533, 534, 539, 543, 557, 563, 573, 574, 580, 610, 612, 618, 626, 642, 644, 651, 668, 673, 677, 683, 686, 690, 691, 692, 694, 699, 722, 725, 731, 732, 754, 755, 780, 787, 812, 814, 815, 835, 854, 862, 866, 867, 873, 875, 887, 910, 914, 930, 951, 952, 963, 965, 981, 994, 995]
-
+ 
+TEST_SCENARIOS= []
 
 
 NETWORK_GRAPH = {
